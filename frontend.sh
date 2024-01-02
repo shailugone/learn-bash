089af70e6f4ec9956ffcbf24b1b6fb985d1e2467
Shylaja Chilukuri <shailu.gone@gmail.com>
2024-01-02 22:10:44 +0530

    new frontend script

--- frontend.sh
+++
@@ -1,9 +0,0 @@
-dnf install nginx -y
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
-systemctl enable nginx
-systemctl restart nginx
